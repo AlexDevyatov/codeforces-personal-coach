c3a17392b23852903b6454f3770d884afc17d46c
alexdevyatov <a.deviatov@ritm.media>
2021-12-05 01:02:11 +0400

    appbar and menu

--- lib/view/first.dart
+++ lib/view/first.dart
@@ -10,49 +10,75 @@ class ProfileRoute extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 48),
-        child: SizedBox.expand(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                width: 150,
-                height: 150,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  border: Border.all(
-                    color: Colors.black,
-                    width: 4
-                  ),
-                  image: DecorationImage(
-                      image: NetworkImage(profile.titlePhoto),
-                      fit: BoxFit.cover),
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Profile info'),
+          automaticallyImplyLeading: false,
+          actions: [
+            PopupMenuButton(
+              icon: Icon(Icons.more_vert),
+              itemBuilder: (context) => [
+                const PopupMenuItem<int>(
+                  value: 0,
+                  child: Text('Reset profile'),
                 ),
-              ),
-              Container(
-                  margin: const EdgeInsets.only(top: 10.0),
-                  child: Text(
-                    profile.handle,
-                    style: getHandleStyle(profile.rating),
-                  )),
-              Container(
-                  margin: const EdgeInsets.only(top: 10.0),
-                  child: Text(
-                    profile.rank,
-                    style: getRankStyle(profile.rating),
-                  )),
-              Container(
-                  margin: const EdgeInsets.only(top: 10.0),
-                  child: Text(
-                    profile.rating.toString(),
-                    style: getRankStyle(profile.rating),
-                  )),
-            ],
+              ],
+              onSelected: (item) => _selectMenuItem(context, item),
+            ),
+          ],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 32),
+          child: SizedBox.expand(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  width: 150,
+                  height: 150,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(color: Colors.black, width: 4),
+                    image: DecorationImage(
+                        image: NetworkImage(profile.titlePhoto),
+                        fit: BoxFit.cover),
+                  ),
+                ),
+                Container(
+                    margin: const EdgeInsets.only(top: 10.0),
+                    child: Text(
+                      profile.handle,
+                      style: getHandleStyle(profile.rating),
+                    )),
+                Container(
+                    margin: const EdgeInsets.only(top: 10.0),
+                    child: Text(
+                      profile.rank,
+                      style: getRankStyle(profile.rating),
+                    )),
+                Container(
+                    margin: const EdgeInsets.only(top: 10.0),
+                    child: Text(
+                      profile.rating.toString(),
+                      style: getRankStyle(profile.rating),
+                    )),
+              ],
+            ),
           ),
         ),
       ),
     );
+  }
+
+  void _selectMenuItem(BuildContext context, item) {
+    switch (item) {
+      case 0:
+        {
+          Navigator.pop(context);
+          break;
+        }
+    }
   }
 }
